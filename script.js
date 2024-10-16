8deae32dcd209502cc13a50ea70fcb59abe14922
Nimish Agarwal <nimishagrawal627@gmail.com>
2024-10-17 00:16:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ cross = true;
 
 audio = new Audio('music.mp3');
 audiogo = new Audio('gameover.mp3');
-setTimeout(() => {
+
     audio.play()
-}, 1000);
+
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
